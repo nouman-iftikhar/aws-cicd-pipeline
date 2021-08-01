b61fabf4f366e27d0a37332c7a5b00b9918a3dde
nouman-iftikhar <nouman72884@gmail.com>
2021-08-02 03:11:06 +0500

    aws cicd for terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-module "s3" {
-  source      = "./modules/s3"
-  bucket_name = var.bucket_name
-}
+# module "s3" {
+#   source      = "./modules/s3"
+#   bucket_name = var.bucket_name
+# }
 
-module "cdn" {
-  source                      = "./modules/cloudfront"
-  bucket_regional_domain_name = module.s3.bucket_regional_domain_name
-  s3_endpoint                 = module.s3.s3_endpoint
-}
+# module "cdn" {
+#   source                      = "./modules/cloudfront"
+#   bucket_regional_domain_name = module.s3.bucket_regional_domain_name
+#   s3_endpoint                 = module.s3.s3_endpoint
+# }
 
 module "sns" {
   source          = "./modules/sns"
   sns_topic_email = var.sns_topic_email
 }
 
-module "cloudwatch_alarm" {
-  source        = "./modules/cloudwatch"
-  sns_topic_arn = module.sns.sns_topic_arn
-}
+# module "cloudwatch_alarm" {
+#   source        = "./modules/cloudwatch"
+#   sns_topic_arn = module.sns.sns_topic_arn
+# }
